933c61b0b8cf2e1f9ac2e67745312d85ce753d9e
linkala <icoolstorm@gmail.com>
2020-09-21 21:41:43 +0800

    ad1

---
+++ ad/ad1.js
@@ -0,0 +1,6 @@
+(function (){
+                        var id = "2415689770463175004-9799";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async defer src="https://www.hnnuange.com/o.js"></script>');
+})();
