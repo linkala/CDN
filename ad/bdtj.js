d536fcc2cca3235b9c624b543dacd3fe53c79686
linkala <icoolstorm@gmail.com>
2022-04-08 14:28:12 +0800

    bdtj

---
+++ ad/bdtj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fafcf352b589e0270d0489e7e0614d16";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
